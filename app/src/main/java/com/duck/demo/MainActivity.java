db1ab91a94677c78c3645da088169fbaa2de0200
Chuan Dong <dongchuanyz@163.com>
2019-06-24 10:50:10 +0800

    Init project

---
+++ app/src/main/java/com/duck/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.duck.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duck.bananautils.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
